2b14b22843950db018d9c6c90f1a85ef6cf30ee2
recife25 <felipe.j.ruiz64@gmail.com>
2020-11-30 17:51:53 -0500

    Added unit test

--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -3,24 +3,39 @@ namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
-        // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // Done Make sure your references are set properly
+        // Righ-click on Dependencies in your .Tests project and set your reference
 
 
 
-        // Create an Add method that passes 3 integers
+        // Done Create an Add method that passes 3 integers
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
 
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
 
         // Create a Multiply method that passes 2 integers
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
 
         // Create a Divide method that passes 2 integers
+        public int Divide(int one, int two)
+        {
+            return one / two;
+        }
 
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
-
         
     }
 }
